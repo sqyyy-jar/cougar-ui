dee205565e61f00a0b4b7460e2e039534b133808
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-21 20:33:08 +0100

    Add Ui interface

---
+++ src/main/kotlin/com/github/sqyyy/cougar/Ui.kt
@@ -0,0 +1,33 @@
+package com.github.sqyyy.cougar
+
+import net.kyori.adventure.text.Component
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.InventoryCloseEvent.Reason
+import org.bukkit.event.inventory.InventoryType
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+
+interface Ui {
+    val type: InventoryType
+    val rows: Int
+    val slots: Int
+    val title: Component
+
+    fun close(player: Player, reason: Reason)
+
+    fun click(slot: Int) // TODO
+
+    fun place(slot: Int) // TODO
+
+    fun placeMany(slots: Set<Int>) // TODO
+
+    fun take(slot: Int) // TODO
+
+    fun replace(slot: Int) // TODO
+
+    fun canPlace(slot: Int): Boolean
+
+    fun canTake(slot: Int): Boolean
+
+    fun nextPlaceableSlot(inventory: Inventory, itemStack: ItemStack): Int
+}
