065e3269682597723d2e52a0f6ac30f141e54752
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-21 19:07:09 +0100

    Add Cougar object class

---
+++ src/main/kotlin/com/github/sqyyy/cougar/Cougar.kt
@@ -0,0 +1,18 @@
+package com.github.sqyyy.cougar
+
+import com.github.sqyyy.cougar.internal.SystemListener
+import org.bukkit.plugin.Plugin
+
+object Cougar {
+    private var initialized = false
+
+    fun initializeSystem(plugin: Plugin) {
+        if (initialized) {
+            throw IllegalStateException("Cougar is already initialized")
+        }
+        plugin.server.pluginManager.run {
+            registerEvents(SystemListener(), plugin)
+        }
+        initialized = true
+    }
+}
