2cd8464859cd0ec1e73e80885c8ae965827d5d6f
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-23 13:51:50 +0100

    Remove old dataset system for testing

--- src/main/kotlin/com/github/sqyyy/cougar/plugin/CougarDatasetPlugin.kt
+++ src/main/kotlin/com/github/sqyyy/cougar/plugin/CougarDatasetPlugin.kt
@@ -1,40 +1,12 @@
 package com.github.sqyyy.cougar.plugin
 
 import com.github.sqyyy.cougar.plugin.listener.InventoryListener
-import net.kyori.adventure.text.Component
 import org.bukkit.plugin.java.JavaPlugin
 
 class CougarDatasetPlugin : JavaPlugin() {
-    private var name: String? = null
-    val dataset = Dataset(hashMapOf())
-
     override fun onEnable() {
         server.pluginManager.also {
             it.registerEvents(InventoryListener(this), this)
         }
-        getCommand("cougar")?.also {
-            it.setExecutor { _, _, _, args ->
-                if (args.isEmpty()) {
-                    this.name = null
-                    return@setExecutor true
-                }
-                this.name = args[0]
-                return@setExecutor true
-            }
-        }
-        getCommand("cougarprint")?.also {
-            it.setExecutor { sender, _, _, args ->
-                if (args.isEmpty()) {
-                    sender.sendMessage(Component.text(dataset.toString()))
-                    return@setExecutor true
-                }
-                sender.sendMessage(Component.text(dataset.datasets[args[0]].toString()))
-                return@setExecutor true
-            }
-        }
-    }
-
-    fun name(): String {
-        return name ?: "no-name"
     }
 }
--- src/main/kotlin/com/github/sqyyy/cougar/plugin/listener/InventoryListener.kt
+++ src/main/kotlin/com/github/sqyyy/cougar/plugin/listener/InventoryListener.kt
@@ -1,11 +1,9 @@
 package com.github.sqyyy.cougar.plugin.listener
 
 import com.github.sqyyy.cougar.plugin.CougarDatasetPlugin
-import com.github.sqyyy.cougar.plugin.Entry
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.inventory.InventoryDragEvent
 
 class InventoryListener(private val cougar: CougarDatasetPlugin) : Listener {
     @EventHandler
@@ -15,24 +13,5 @@ class InventoryListener(private val cougar: CougarDatasetPlugin) : Listener {
 Event: ${event.eventName}:${event.action}:${event.rawSlot}:${event.currentItem?.type}:${event.cursor?.type}
             """.trim()
         )
-        val dataset = cougar.dataset.datasets.getOrPut(cougar.name()) { mutableListOf() }
-        if (dataset.lastOrNull()?.obj == event) {
-            return
-        }
-        val entry = Entry(
-            event, event.eventName, event.action, event.click, event.slotType, event.isLeftClick,
-            event.isRightClick, event.isShiftClick
-        )
-        dataset += entry
-    }
-
-    @EventHandler
-    fun on(event: InventoryDragEvent) {
-        val dataset = cougar.dataset.datasets.getOrPut(cougar.name()) { mutableListOf() }
-        if (dataset.lastOrNull()?.obj == event) {
-            return
-        }
-        val entry = Entry(event, event.eventName, dragType = event.type)
-        dataset += entry
     }
 }
