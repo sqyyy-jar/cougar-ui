fd50d561a1d68691b452a1c6bfdd56d1f9227e8d
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-23 19:42:44 +0100

    Improve ClickPanel

--- src/main/kotlin/com/github/sqyyy/cougar/impl/ClickPanel.kt
+++ src/main/kotlin/com/github/sqyyy/cougar/impl/ClickPanel.kt
@@ -1,27 +1,28 @@
 package com.github.sqyyy.cougar.impl
 
 import com.github.sqyyy.cougar.Panel
+import com.github.sqyyy.cougar.Slot
 import org.bukkit.entity.Player
-import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.InventoryView
 
 class ClickPanel(
     private val startSlot: Int,
     private val endSlot: Int,
     private val callback: (player: Player, view: InventoryView, slot: Int) -> Boolean,
-    private val type: InventoryType,
+    private val rowWidth: Int,
 ) : Panel {
-    private val scale = if (this.type == InventoryType.CHEST) 9 else 3
-    private val startColumn = this.startSlot % this.scale
-    private val startRow = this.startSlot - this.startColumn
-    private val endColumn = this.endSlot % this.scale
-    private val endRow = this.endSlot - this.endColumn
+    private val startRow = Slot.getRow(this.rowWidth, this.startSlot)
+    private val startColumn = Slot.getColumn(this.rowWidth, this.startSlot)
+    private val endRow = Slot.getRow(this.rowWidth, this.endSlot)
+    private val endColumn = Slot.getColumn(this.rowWidth, this.endSlot)
 
-    override fun collidesWith(slot: Int): Boolean = this.startSlot <= slot && slot <= this.endSlot
-
-    override fun click(player: Player, view: InventoryView, slot: Int): Boolean {
-        return this.callback(player, view, slot)
+    override fun collidesWith(slot: Int): Boolean {
+        val column: Int = Slot.getColumn(this.rowWidth, slot)
+        val row: Int = Slot.getColumn(this.rowWidth, slot)
+        return row in this.startRow..this.endRow && column >= this.startColumn && column <= this.endColumn
     }
+
+    override fun click(player: Player, view: InventoryView, slot: Int): Boolean = this.callback(player, view, slot)
 
     override fun canClick(slot: Int): Boolean = true
 
