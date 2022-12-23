e5041fd2b918fd83635e1ba9c4ace283900c67b2
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-23 13:50:50 +0100

    Update UiHolder including click callback

--- src/main/kotlin/com/github/sqyyy/cougar/internal/UiHolder.kt
+++ src/main/kotlin/com/github/sqyyy/cougar/internal/UiHolder.kt
@@ -1,6 +1,7 @@
 package com.github.sqyyy.cougar.internal
 
 import com.github.sqyyy.cougar.Ui
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryAction
 import org.bukkit.event.inventory.InventoryClickEvent
@@ -52,6 +53,9 @@ class UiHolder(private val ui: Ui) : InventoryHolder {
                 }
                 if (!ui.canTake(slot)) {
                     event.isCancelled = true
+                    if (ui.canClick(slot)) {
+                        ui.click(slot)
+                    }
                     return
                 }
                 ui.take(slot)
@@ -65,46 +69,109 @@ class UiHolder(private val ui: Ui) : InventoryHolder {
                 }
                 if (!ui.canPlace(slot)) {
                     event.isCancelled = true
+                    if (ui.canClick(slot)) {
+                        ui.click(slot)
+                    }
                     return
                 }
                 ui.place(slot)
             }
             InventoryAction.SWAP_WITH_CURSOR,
             InventoryAction.HOTBAR_MOVE_AND_READD,
-            InventoryAction.HOTBAR_SWAP,
             -> {
                 if (!uiClick) {
                     return
                 }
                 if (!ui.canTake(slot) || !ui.canPlace(slot)) {
                     event.isCancelled = true
+                    if (ui.canClick(slot)) {
+                        ui.click(slot)
+                    }
                     return
                 }
                 ui.replace(slot)
+            }
+            InventoryAction.HOTBAR_SWAP -> {
+                if (!uiClick) {
+                    return
+                }
+                val place = event.currentItem?.type == Material.AIR
+                if (place) {
+                    if (!ui.canPlace(slot)) {
+                        event.isCancelled = true
+                        if (ui.canClick(slot)) {
+                            ui.click(slot)
+                        }
+                        return
+                    }
+                    ui.place(slot)
+                    return
+                }
+                if (!ui.canTake(slot)) {
+                    event.isCancelled = true
+                    if (ui.canClick(slot)) {
+                        ui.click(slot)
+                    }
+                    return
+                }
+                ui.take(slot)
             }
             InventoryAction.MOVE_TO_OTHER_INVENTORY -> {
                 if (event.currentItem == null) {
                     return
                 }
+                val currentItem = event.currentItem!!
+                if (currentItem.type == Material.AIR) {
+                    return
+                }
                 if (uiClick) {
                     if (!ui.canTake(slot)) {
                         event.isCancelled = true
+                        if (ui.canClick(slot)) {
+                            ui.click(slot)
+                        }
                         return
                     }
                     ui.take(slot)
                     return
                 }
-                val nextSlot = ui.nextPlaceableSlot(inventory!!, event.currentItem!!)
-                if (nextSlot == -1) {
-                    event.isCancelled = true
-                    return
+                val inventory = inventory!!
+                var amount = currentItem.amount
+                for (i in 0 until ui.slots) {
+                    if (!ui.canPlace(i)) {
+                        continue
+                    }
+                    val item = inventory.getItem(i)
+                    if (item == null || item.type == Material.AIR) {
+                        event.isCancelled = true
+                        event.view.bottomInventory.setItem(slot, null)
+                        inventory.setItem(i, currentItem)
+                        break
+                    }
+                    if (item.amount >= 64) {
+                        continue
+                    }
+                    if (currentItem.isSimilar(item)) {
+                        val max = 64 - item.amount
+                        if (max >= amount) {
+                            event.isCancelled = true
+                            event.view.bottomInventory.setItem(slot, null)
+                            inventory.setItem(i, currentItem)
+                            break
+                        }
+                        amount -= max
+                        event.isCancelled = true
+                        item.amount = 64
+                        currentItem.amount = amount
+                        break
+                    }
                 }
-                TODO("Find next placeable slot and put item into it")
             }
-            InventoryAction.COLLECT_TO_CURSOR -> {
+            InventoryAction.COLLECT_TO_CURSOR,
+            InventoryAction.UNKNOWN,
+            -> {
                 event.isCancelled = true
             }
-            InventoryAction.UNKNOWN -> TODO("Maybe cancel this for now")
             else -> {}
         }
     }
