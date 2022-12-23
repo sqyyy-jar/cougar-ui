76a99a5d3a3f1be3630b5d944730bf7db33ca26d
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-23 19:11:35 +0100

    Add SingleSlotClickPanel

---
+++ src/main/kotlin/com/github/sqyyy/cougar/impl/SingleSlotClickPanel.kt
@@ -0,0 +1,22 @@
+package com.github.sqyyy.cougar.impl
+
+import com.github.sqyyy.cougar.Panel
+import org.bukkit.entity.Player
+import org.bukkit.inventory.InventoryView
+
+class SingleSlotClickPanel(
+    private val slot: Int,
+    private val callback: (player: Player, view: InventoryView, slot: Int) -> Boolean,
+) : Panel {
+    override fun collidesWith(slot: Int): Boolean = slot == this.slot
+
+    override fun click(player: Player, view: InventoryView, slot: Int): Boolean {
+        return this.callback(player, view, slot)
+    }
+
+    override fun canClick(slot: Int): Boolean = true
+
+    override fun canPlace(slot: Int): Boolean = false
+
+    override fun canTake(slot: Int): Boolean = false
+}
