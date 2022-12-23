9440f7449fb92ebcd883eef4be4ffa2e4aa7e761
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-23 13:49:11 +0100

    Update InventoryListener

--- src/main/kotlin/com/github/sqyyy/cougar/plugin/listener/InventoryListener.kt
+++ src/main/kotlin/com/github/sqyyy/cougar/plugin/listener/InventoryListener.kt
@@ -1,29 +1,20 @@
 package com.github.sqyyy.cougar.plugin.listener
 
-import com.destroystokyo.paper.event.inventory.PrepareResultEvent
 import com.github.sqyyy.cougar.plugin.CougarDatasetPlugin
 import com.github.sqyyy.cougar.plugin.Entry
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.inventory.InventoryCreativeEvent
 import org.bukkit.event.inventory.InventoryDragEvent
-import org.bukkit.event.inventory.InventoryEvent
-import org.bukkit.event.inventory.InventoryInteractEvent
 
 class InventoryListener(private val cougar: CougarDatasetPlugin) : Listener {
     @EventHandler
-    fun on(event: InventoryInteractEvent) {
-        val dataset = cougar.dataset.datasets.getOrPut(cougar.name()) { mutableListOf() }
-        if (dataset.lastOrNull()?.obj == event) {
-            return
-        }
-        val entry = Entry(event, event.eventName)
-        dataset += entry
-    }
-
-    @EventHandler
     fun on(event: InventoryClickEvent) {
+        event.whoClicked.sendMessage(
+            """
+Event: ${event.eventName}:${event.action}:${event.rawSlot}:${event.currentItem?.type}:${event.cursor?.type}
+            """.trim()
+        )
         val dataset = cougar.dataset.datasets.getOrPut(cougar.name()) { mutableListOf() }
         if (dataset.lastOrNull()?.obj == event) {
             return
@@ -42,39 +33,6 @@ class InventoryListener(private val cougar: CougarDatasetPlugin) : Listener {
             return
         }
         val entry = Entry(event, event.eventName, dragType = event.type)
-        dataset += entry
-    }
-
-    @EventHandler
-    fun on(event: InventoryCreativeEvent) {
-        val dataset = cougar.dataset.datasets.getOrPut(cougar.name()) { mutableListOf() }
-        if (dataset.lastOrNull()?.obj == event) {
-            return
-        }
-        val entry = Entry(
-            event, event.eventName, event.action, event.click, event.slotType, event.isLeftClick,
-            event.isRightClick, event.isShiftClick
-        )
-        dataset += entry
-    }
-
-    @EventHandler
-    fun on(event: PrepareResultEvent) {
-        val dataset = cougar.dataset.datasets.getOrPut(cougar.name()) { mutableListOf() }
-        if (dataset.lastOrNull()?.obj == event) {
-            return
-        }
-        val entry = Entry(event, event.eventName)
-        dataset += entry
-    }
-
-    @EventHandler
-    fun on(event: InventoryEvent) {
-        val dataset = cougar.dataset.datasets.getOrPut(cougar.name()) { mutableListOf() }
-        if (dataset.lastOrNull()?.obj == event) {
-            return
-        }
-        val entry = Entry(event, event.eventName)
         dataset += entry
     }
 }
