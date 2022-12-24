7fb2eb1e729e7203c1295cf9ada5abcddbbdef11
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-24 13:27:53 +0100

    Add Cougar#scheduleTask method

--- src/main/kotlin/com/github/sqyyy/cougar/Cougar.kt
+++ src/main/kotlin/com/github/sqyyy/cougar/Cougar.kt
@@ -5,15 +5,25 @@ import org.bukkit.plugin.Plugin
 
 object Cougar {
     private var initialized = false
+    private var plugin: Plugin? = null
 
     @JvmStatic
     fun initializeSystem(plugin: Plugin) {
         if (initialized) {
             throw IllegalStateException("Cougar is already initialized")
         }
+        this.plugin = plugin
         plugin.server.pluginManager.run {
             registerEvents(SystemListener(), plugin)
         }
         initialized = true
+    }
+
+    @JvmStatic
+    internal fun scheduleTask(task: Runnable) {
+        if (plugin == null) {
+            throw IllegalStateException("Cougar is not yet initialized")
+        }
+        plugin!!.server.scheduler.runTask(plugin!!, task)
     }
 }
