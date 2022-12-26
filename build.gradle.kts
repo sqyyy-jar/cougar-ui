ffb762a83b1b18b70ea11e5246ee086d21cf6a7c
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-26 13:00:40 +0100

    Fix Gradle workflow configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,29 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.22"
+    `maven-publish`
 }
 
 group = "com.github.sqyyy"
 version = "0.1.0-alpha"
+
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/liquip/liquip-plugin")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
 
 repositories {
     mavenCentral()
