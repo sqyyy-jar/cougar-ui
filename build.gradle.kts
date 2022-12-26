a08fa7db56ad43cc7ae445c200f74b646709fc66
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-26 13:06:25 +0100

    Fix Gradle workflow configuration again

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ configure<PublishingExtension> {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/liquip/liquip-plugin")
+            url = uri("https://maven.pkg.github.com/sqyyy-jar/cougar-ui")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
