ba0e17e92aa91f2b49dd3405c1dde66f5c741954
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-29 13:28:34 +0100

    Make AbstractSlotPanel#slot protected

--- src/main/kotlin/com/github/sqyyy/cougar/impl/panel/AbstractSlotPanel.kt
+++ src/main/kotlin/com/github/sqyyy/cougar/impl/panel/AbstractSlotPanel.kt
@@ -2,6 +2,6 @@ package com.github.sqyyy.cougar.impl.panel
 
 import com.github.sqyyy.cougar.Panel
 
-abstract class AbstractSlotPanel(private val slot: Int) : Panel {
+abstract class AbstractSlotPanel(protected val slot: Int) : Panel {
     override fun collidesWith(slot: Int): Boolean = slot == this.slot
 }
